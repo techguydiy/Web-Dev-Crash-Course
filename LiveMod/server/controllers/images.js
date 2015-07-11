10322e3ad99987e65ac701c819dce9aefd4e139f
techguydiy <jason@techguydiy.com>
2015-07-11 10:27:39 -0500

    Yesterdays tweaks

--- LiveMod/server/controllers/images.js
+++ LiveMod/server/controllers/images.js
@@ -1,26 +1,61 @@
-var imageModel = require('../models/imageModel');
+var imageModel = require('../models/images');
 
 
 exports.postDecision = function(req, res){
 
-	res.send({message:"Decision Posted!"});
+	imageModel.update({_id:req.params.image_id}, {approved: req.body.decision}, function(err, num, raw){
+		if(err){
+			res.send(err);
+		}
+
+		res.json({message: num + ' updated'});
+	});
 
 }
 
 exports.postImage = function(req, res){
 
-	res.json({message:"posted an image"});
+	console.log(req);
+
+	var image = new imageModel();
+
+	image.imageTitle = req.body.imageTitle;
+	image.imageDesc = req.body.imageDesc;
+	image.imagePath = req.body.imagePath;
+	image.approved = 0;
+	image.uploadedDate = req.body.uploadDate;
+
+	image.save(function(err){
+		if(err){
+			res.send(err);
+		}
+
+		res.json({message: 'Image was saved to the database', data:image});
+
+	});
 
 }
 
 exports.getImage = function(req, res){
 
-	res.json({message:"got an image"});
+	imageModel.find({ _id: req.params.image_id}, function(err, image){
+		if(err){
+			res.send(err);
+		}
+
+		res.json(image);
+	});
 
 }
 
 exports.getImages = function(req, res){
 
-	res.json({message:"got all the images"});
+	imageModel.find({}, function(err, images){
+		if(err){
+			res.send(err);
+		}
+
+		res.json(images);
+	});
 
 }
--- LiveMod/server/server.js
+++ LiveMod/server/server.js
@@ -1,27 +1,50 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+
 var imageController = require('./controllers/images.js');
 var adminController = require('./controllers/admin.js');
 
 mongoose.connect('mongodb://localhost:27017/liveMod');
 
 var app = express();
-
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var totalUsers = 0;
 app.use(bodyParser.urlencoded({
 	extended:true
 }));
+
+app.get('/', function(req, res, next){
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	totalUsers++;
+	console.log('a user has connected - connect users = ' + totalUsers);
+	//totalUsers = totalUser + 1;
+
+	socket.on('disconnect', function(){
+		totalUsers--;
+		console.log("user disonnected - connected users = " + totalUsers);
+		
+	});
+});
+
+
 
 var router = express.Router();
 
 router.route('/images')
 	.get(imageController.getImages);
 
-router.route('/image')
-	.post(imageController.postImage)
+router.route('/image/:image_id')
 	.get(imageController.getImage);
 
-router.route('/decision')
+router.route('/image')
+	.post(imageController.postImage);
+
+router.route('/decision/:image_id')
 	.post(imageController.postDecision);
 
 router.route('/admin/event')
@@ -34,4 +57,4 @@ router.route('/admin/getConnections')
 
 app.use('/api', router);
 
-app.listen(3000);
+server.listen(3000);
